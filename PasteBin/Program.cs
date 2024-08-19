3be5f9022053861c2b6e40c3466bb0e3c423da4c
whoswhip <whoswhip@gmail.com>
2024-08-18 23:50:58 -0400

    Last commit didnt add everything :(

--- PasteBin/Program.cs
+++ PasteBin/Program.cs
@@ -4,6 +4,10 @@ using System.Threading.RateLimiting;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Microsoft.AspNetCore.Components.Web;
+using System.Data;
+using System.Net.WebSockets;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System.Drawing;
 
 class Program
 {
@@ -15,7 +19,7 @@ class Program
             {
                 await connection.OpenAsync();
                 var command = connection.CreateCommand();
-                command.CommandText = "CREATE TABLE pastes (UID INTEGER,Title TEXT, Date TEXT, Size TEXT, ID TEXT, PRIMARY KEY(UID AUTOINCREMENT))";
+                command.CommandText = "CREATE TABLE pastes (UID INTEGER,Title TEXT, Date TEXT, Size TEXT, Visibility TEXT, ID TEXT, PRIMARY KEY(UID AUTOINCREMENT))";
                 await command.ExecuteNonQueryAsync();
             }
         }
@@ -54,11 +58,23 @@ class Program
 
         app.MapRazorPages();
 
+        #region Frontend
+
         app.MapGet("/", async (HttpContext context) =>
         {
             var html = File.ReadAllText("assets\\index.html");
             context.Response.ContentType = "text/html";
             await context.Response.WriteAsync(html);
+            return;
+        });
+        app.MapGet("/paste-not-found", async (HttpContext context) =>
+        {
+            context.Response.StatusCode = 404;
+            var error_html = File.ReadAllText("assets\\error.html");
+            error_html = error_html.Replace("{code}", "404");
+            error_html = error_html.Replace("{message}", "Paste not found");
+            context.Response.ContentType = "text/html";
+            await context.Response.WriteAsync(error_html);
             return;
         });
         app.MapGet("/{id}", async (HttpContext context) =>
@@ -70,6 +86,7 @@ class Program
                 return;
             }
             var content = File.ReadAllText($"pastes/{id}.txt");
+            content = content.Replace("\n", "<br>").Replace("\r", "");
             using (var connection = new SqliteConnection("Data Source=pastes.sqlite"))
             {
                 await connection.OpenAsync();
@@ -78,13 +95,26 @@ class Program
                 command.Parameters.AddWithValue("@ID", id);
                 var reader = await command.ExecuteReaderAsync();
                 await reader.ReadAsync();
+                if(!reader.HasRows)
+                {
+                    context.Response.StatusCode = 404;
+                    context.Response.Redirect("/paste-not-found");
+                    return;
+                }
                 var paste = new Paste
                 {
                     Title = reader.GetString(1),
                     Date = reader.GetString(2),
                     Size = reader.GetString(3),
+                    Visibility = reader.GetString(4),
                     Id = id,
                 };
+                if(paste.Visibility == "Private")
+                {
+                    // WIP Private pastes will be implemented soon enough, most likely with a password or longer ID
+                    context.Response.StatusCode = 404;
+                    return;
+                }
                 var html = File.ReadAllText("assets\\paste.html");
                 html = html.Replace("{pastetitle}", paste.Title);
                 html = html.Replace("{content}", content);
@@ -107,10 +137,51 @@ class Program
             await context.Response.WriteAsync(content);
             return;
         });
+        app.MapGet("/pastes", async (HttpContext context) =>
+        {
+            var html = File.ReadAllText("assets\\recent_pastes.html");
+            using (var connection = new SqliteConnection("Data Source=pastes.sqlite"))
+            {
+                await connection.OpenAsync();
+                var command = connection.CreateCommand();
+                command.CommandText = "SELECT * FROM pastes ORDER BY UID DESC LIMIT 10";
+                var reader = await command.ExecuteReaderAsync();
+                string pastes = string.Empty;
+                while (await reader.ReadAsync())
+                {
+                    var paste = new Paste
+                    {
+                        Title = reader.GetString(1),
+                        Date = reader.GetString(2),
+                        Size = reader.GetString(3),
+                        Visibility = reader.GetString(4),
+                        Id = reader.GetString(5),
+                    };
+                    if(paste.Visibility == "Private" || paste.Visibility == "Unlisted")
+                    {
+                        continue;
+                    }
+                    pastes += $"<a href=\"/{paste.Id}\"><li>{paste.Title} {paste.Date} {ConvertToBytes(paste.Size)}</li></a>";
+                }
+                if(string.IsNullOrEmpty(pastes))
+                {
+                    pastes = "<h2>No pastes found</h1>";
+                }
+                html = html.Replace("{pastes}", pastes);
+                context.Response.ContentType = "text/html";
+                await context.Response.WriteAsync(html);
+                return;
+            }
+        }).RequireRateLimiting("fixed");
+
+        #endregion
+
+        #region API
+
         app.MapPost("/api/create", async (HttpContext context) =>
         {
             string requestBody = await new StreamReader(context.Request.Body).ReadToEndAsync();
-            if (requestBody.Length > 10000 || string.IsNullOrEmpty(requestBody))
+            if (requestBody.Length > 500_000 || string.IsNullOrEmpty(requestBody))
             {
                 context.Response.StatusCode = 400;
                 context.Response.ContentType = "application/json";
@@ -120,7 +191,16 @@ class Program
             var data = JObject.Parse(requestBody);
             var title = SanitizeInput(data["title"]?.ToString() ?? "Untitled");
             var content = SanitizeInput(data["content"]?.ToString() ?? "");
-            if(string.IsNullOrEmpty(content))
+            var visibility = SanitizeInput(data["visibility"]?.ToString() ?? "Public");
+
+            if(visibility != "Public" && visibility != "Unlisted" && visibility != "Private")
+            {
+                context.Response.StatusCode = 400;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(new JObject { ["error"] = "Invalid visibility" }.ToString());
+                return;
+            }
+            if(string.IsNullOrEmpty(content) || string.IsNullOrWhiteSpace(content))
             {
                 context.Response.StatusCode = 400;
                 context.Response.ContentType = "application/json";
@@ -128,11 +208,13 @@ class Program
                 return;
             }
 
-            var paste = await CreatePaste(content, title);
+            var paste = await CreatePaste(content, title, visibility);
 
             if (!File.Exists($"pastes/{paste?.Id}.txt") || paste == null)
             {
                 context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+                await context.Response.WriteAsJsonAsync(new JObject { ["error"] = "Failed to create paste" }.ToString());
                 return;
             }
 
@@ -141,36 +223,41 @@ class Program
 
             JObject responseJson = new JObject();
             responseJson["pasteId"] = paste.Id;
-            responseJson["pasteSize"] = paste.Size;
+            responseJson["pasteSize"] = ConvertToBytes(paste.Size ?? string.Empty);
             responseJson["pasteDate"] = paste.Date;
+            responseJson["pasteVisibility"] = paste.Visibility;
 
             await context.Response.WriteAsJsonAsync(responseJson.ToString());
             return;
 
         }).RequireRateLimiting("fixed");
 
+        #endregion
+
         await app.RunAsync();
     }
 
-    public static async Task<Paste> CreatePaste(string content, string title)
+    public static async Task<Paste> CreatePaste(string content, string title, string visibility)
     {
         var paste = new Paste
         {
             Content = content,
             Title = title,
             Date = DateTime.Now.ToString("dd/MM/yyyy-HH:mm:ss"),
-            Size = ConvertToBytes(content.Length.ToString()),
+            Size = content.Length.ToString(),
+            Visibility = visibility,
             Id = GenerateRandomString(12)
         };
-        File.WriteAllText($"pastes/{paste.Id}.txt", paste.Content);
+        await File.WriteAllTextAsync($"pastes/{paste.Id}.txt", paste.Content);
         using (var connection = new SqliteConnection("Data Source=pastes.sqlite"))
         {
             await connection.OpenAsync();
             var command = connection.CreateCommand();
-            command.CommandText = "INSERT INTO pastes (Title, Date, Size, ID) VALUES (@Title, @Date, @Size, @ID)";
+            command.CommandText = "INSERT INTO pastes (Title, Date, Size, Visibility, ID) VALUES (@Title, @Date, @Size, @Visibility, @ID)";
             command.Parameters.AddWithValue("@Title", paste.Title);
             command.Parameters.AddWithValue("@Date", paste.Date);
             command.Parameters.AddWithValue("@Size", paste.Size);
+            command.Parameters.AddWithValue("@Visibility", paste.Visibility);
             command.Parameters.AddWithValue("@ID", paste.Id);
             await command.ExecuteNonQueryAsync();
         }
@@ -183,6 +270,14 @@ class Program
     }
     public static string ConvertToBytes(string length)
     {
+        try
+        {
+            double.Parse(length);
+        }
+        catch
+        { 
+            return "0 B"; 
+        }
         string[] sizes = { "B", "KB", "MB", "GB", "TB" };
         double len = double.Parse(length);
         int order = 0;
@@ -207,5 +302,6 @@ public class Paste
     public string? Content { get; set; }
     public string? Size { get; set; }
     public string? Date { get; set; }
+    public string? Visibility { get; set; }
     public string? Id { get; set; }
 }
