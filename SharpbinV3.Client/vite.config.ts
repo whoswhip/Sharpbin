eca6867fcab5c1ba4d1d327c76f18fd21290abd6
whoswhip <contact@whoswhip.dev>
2026-01-20 09:13:27 -0500

    feat: add application version definition to Vite config

--- SharpbinV3.Client/vite.config.ts
+++ SharpbinV3.Client/vite.config.ts
@@ -1,6 +1,12 @@
 import tailwindcss from '@tailwindcss/vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import { readFileSync } from 'fs';
+import { fileURLToPath } from 'url';
+
+const file = fileURLToPath(new URL('package.json', import.meta.url));
+const json = readFileSync(file, 'utf8');
+const pkg = JSON.parse(json);
 
 const apiUrl = process.env.VITE_API_URL || 'http://localhost:5050';
 const allowedHosts = process.env.VITE_ALLOWED_HOSTS
@@ -9,6 +15,9 @@ const allowedHosts = process.env.VITE_ALLOWED_HOSTS
 
 export default defineConfig({
 	plugins: [tailwindcss(), sveltekit()],
+	define: {
+		__APP_VERSION__: JSON.stringify(pkg.version)
+	},
 	server: {
 		host: true,
 		proxy: {
