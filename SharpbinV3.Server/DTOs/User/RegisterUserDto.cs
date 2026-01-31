71363fa1a467c660297301d4f35513bdfe39ccd5
whoswhip <contact@whoswhip.dev>
2026-01-31 18:27:18 -0500

    feat(backend): allow spaces in display name during registration

--- SharpbinV3.Server/DTOs/User/RegisterUserDto.cs
+++ SharpbinV3.Server/DTOs/User/RegisterUserDto.cs
@@ -30,8 +30,8 @@ namespace SharpbinV3.Server.DTOs
             {
                 if (DisplayName.Length < 3 || DisplayName.Length > 26)
                     results.Add(new ValidationResult("DisplayName must be between 3 and 26 characters.", [nameof(DisplayName)]));
-                if (!ValidateUsername().IsMatch(DisplayName))
-                    results.Add(new ValidationResult("DisplayName can only contain letters, numbers, underscores, and dots.", [nameof(DisplayName)]));
+                if (!ValidateDisplayname().IsMatch(DisplayName))
+                    results.Add(new ValidationResult("DisplayName can only contain alphanumeric characters, underscores, periods, and spaces (no leading or trailing spaces).", [nameof(DisplayName)]));
             }
             if (Email != null)
             {
@@ -44,6 +44,8 @@ namespace SharpbinV3.Server.DTOs
 
         [GeneratedRegex("^[A-Za-z0-9_.]+$")]
         private static partial Regex ValidateUsername();
+        [GeneratedRegex("^[A-Za-z0-9_.]+(?: [A-Za-z0-9_.]+)*$")]
+        private static partial Regex ValidateDisplayname();
         [GeneratedRegex("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{8,}$")]
         public static partial Regex ValidatePassword();
     }
