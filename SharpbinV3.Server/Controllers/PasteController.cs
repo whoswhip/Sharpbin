f70328c93d8b008f6015ec239cc984bbcc0b57e3
whoswhip <contact@whoswhip.dev>
2026-03-10 19:06:50 -0400

    fix(controller): only return paste not found if null or expired

--- SharpbinV3.Server/Controllers/PasteController.cs
+++ SharpbinV3.Server/Controllers/PasteController.cs
@@ -96,7 +96,7 @@ namespace SharpbinV3.Server.Controllers
         public async Task<IActionResult> GetPasteByID(string id)
         {
             var paste = await _pasteService.Get(id);
-            if (paste == null || (paste.ExpiresAt < DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() && paste.Visibility != 0))
+            if (paste == null || paste.ExpiresAt < DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
                 return NotFound(new { success = false, message = "Paste not found" });
 
             var jwtUser = HttpContext.GetJwtUser();
@@ -150,7 +150,7 @@ namespace SharpbinV3.Server.Controllers
         public async Task<IActionResult> GetRawPasteByID(string id)
         {
             var paste = await _pasteService.Get(id);
-            if (paste == null || (paste.ExpiresAt < DateTimeOffset.UtcNow.ToUnixTimeMilliseconds() && paste.Visibility != 0))
+            if (paste == null || paste.ExpiresAt < DateTimeOffset.UtcNow.ToUnixTimeMilliseconds())
                 return NotFound();
             if (paste.IsCompressed)
                 Response.Headers.Append("Content-Encoding", "gzip");
