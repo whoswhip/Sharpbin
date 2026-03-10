873b1778ee5ca246451d8cd391f9ad74b9325fd8
whoswhip <contact@whoswhip.dev>
2026-03-10 03:23:44 -0400

    refactor(migrations): correct namespace for entities in model snapshot

---
+++ SharpbinV3.Server/Migrations/20260310072309_CorrectNamespaceRename.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SharpbinV3.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class CorrectNamespaceRename : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
