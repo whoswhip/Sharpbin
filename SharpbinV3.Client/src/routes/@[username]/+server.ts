70e637b281ae060e40ceb6e312591276d1c68540
whoswhip <contact@whoswhip.dev>
2026-04-25 03:12:12 -0400

    feat: add /@username redirect to /user/username

---
+++ SharpbinV3.Client/src/routes/@[username]/+server.ts
@@ -0,0 +1,7 @@
+import { error, redirect } from '@sveltejs/kit';
+
+export async function GET({ params }) {
+    const username = params?.username;
+    if (!username) throw error(404, 'User not found');
+    throw redirect(302, `/user/${encodeURIComponent(username)}`);
+}
